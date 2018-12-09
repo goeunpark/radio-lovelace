a429553323f23423a4919d867167bad99c3dacac
Goeun Park <goeuntothepark@gmail.com>
2018-12-09 15:27:28 -0800

    make songData in radio set into this.state.tracks

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import songData from './data/tracks.json';
 
 songData.forEach((song, i) => {
   song.id = i;
+  song.favorite = false;
 });
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tracks: songData,
+    };
+  };
+
   render() {
     return (
       <div className="App">
@@ -17,9 +25,11 @@ class App extends Component {
           <h1 className="page-header--title">Radio Lovelace</h1>
         </header>
         <main className="main">
-          <RadioSet tracks={songData} />
+          
+          <RadioSet tracks={this.state.tracks} />
         </main>
       </div>
+
     );
   }
 }
